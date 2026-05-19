e5481e9ca0e5a9f1bf2a5f2aa6a9f8e03377d179
Triler1 <q56875108@gmail.com>
2026-05-19 15:09:02 +0300

    Change title to version B

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from calculator import add, subtract, multiply
 
 
 def main():
-    print("Консольное приложение-калькулятор")
+    print("Калькулятор версия B")
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
     print("4 * 3 =", multiply(4, 3))
