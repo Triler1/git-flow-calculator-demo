8071b51ff3b44ce3c9a24c0e9cc1a8967737fd86
Triler1 <q56875108@gmail.com>
2026-05-19 14:01:14 +0300

    Initial project structure

---
+++ main.py
@@ -0,0 +1,11 @@
+from calculator import add, subtract
+
+
+def main():
+    print("Калькулятор")
+    print("2 + 3 =", add(2, 3))
+    print("5 - 2 =", subtract(5, 2))
+
+
+if __name__ == "__main__":
+    main()
